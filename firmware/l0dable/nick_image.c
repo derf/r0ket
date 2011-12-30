b06c4a01124e3811a96a668adccf2dd04e92ca93
Daniel Friesel <derf@finalrewind.org>
2011-12-30 14:34:39 +0100

    nick_image: special charge screen with animations

--- firmware/l0dable/nick_image.c
+++ firmware/l0dable/nick_image.c
@@ -8,7 +8,7 @@
 
 #include "usetable.h"
 
-void ledwobble(void);
+void ledwobble(char mode);
 
 const unsigned char pwm[32] = {
 	0, 2, 3, 4, 6, 8, 11, 16, 23, 32, 45, 64,  90, 128, 181, 255,
@@ -18,8 +18,9 @@ const unsigned char pwm[32] = {
 void ram(void)
 {
 	unsigned char t = 0;
-	int mv;
-	unsigned char percent;
+	unsigned int raw_mv = 0, mv = 0;
+	int not_charging;
+	unsigned int percent;
 	char key;
 	lcdLoadImage("sr.lcd");
 	lcdRefresh();
@@ -29,23 +30,43 @@ void ram(void)
 	gpioSetDir(RB_LED3, gpioDirection_Output);
 	gpioSetDir(1, 3, gpioDirection_Output);
 
+	mv = GetVoltage();
+
 	for (;;) {
 		if (++t >= 230) {
-			mv = GetVoltage();
+			raw_mv = GetVoltage();
+			not_charging = gpioGetValue(RB_PWR_CHRG);
+
+			if (!not_charging)
+				raw_mv -= 500;
+
+			mv = ((mv * 3) + raw_mv) / 4;
+
 			percent = (mv - 3450) * 100 / (4100 - 3450);
 			setExtFont(GLOBAL(nickfont));
 			lcdClear();
 			DoString(0,0, GLOBAL(nickname));
-			DoString(0, 40, (percent > 100 ? "1" : ""));
-			DoString(16, 40, IntToStr(percent % 100, 2, F_ZEROS));
-			DoString(50, 40, "%");
+
+			if (!not_charging)
+				DoString(0, 40, "+");
+			else if (not_charging && (raw_mv > 4200)) {
+				DoString(0, 40, "=");
+			}
+
+			DoString(16, 40, (percent > 100 ? "1" : ""));
+			DoString(32, 40, IntToStr(percent % 100, 2, F_ZEROS | F_LONG));
+			DoString(64, 40, "%");
 			lcdRefresh();
 
-			ledwobble();
+			ledwobble((not_charging && (raw_mv < 4200)));
 
-			t = 0;
-			lcdLoadImage("sr.lcd");
-			lcdRefresh();
+			if (not_charging && (raw_mv < 4200)) {
+				t = 0;
+				lcdLoadImage("sr.lcd");
+				lcdRefresh();
+			}
+			else
+				t = 233;
 		}
 
 		delayms(80);
@@ -75,43 +96,56 @@ void ram(void)
 	}
 };
 
-void ledwobble(void)
+void ledwobble(char mode)
 {
+	char key;
 	unsigned char i;
 	unsigned int cnt = 0;
-	unsigned int x = 0;
+	unsigned char x = 0;
 	unsigned char fin = 0;
 	unsigned const int cnt_max = 2560;
-	unsigned const char x_max = 32;
+	unsigned const char x_max[2] = {32, 48};
 	unsigned char step;
 
-	unsigned char led[4];
-	unsigned char f_led[4][32];
+	unsigned char led[4] = {0, 0, 0, 0};
 
 	for (;;) {
 		if (++cnt >= cnt_max) {
 			cnt = 0;
-			if (++x == x_max) {
+			if (++x == x_max[mode]) {
 				x = 0;
-				if (++fin == 23)
+				delayms(500);
+
+				key = getInputRaw();
+				if (key == BTN_ENTER)
+					return;
+
+				if (++fin == 5)
 					return;
 			}
 
-			for (i = 0; i < 4; i++) {
-				led[0] = pwm[x];
-				led[1] = pwm[x];
-				led[2] = pwm[(x+16) % 32];
-				led[3] = pwm[(x+16) % 32];
+			if (mode == 1) {
+				if (x < 32)
+					led[0] = led[2] = pwm[x];
+				if (x > 15)
+					led[1] = led[3] = pwm[x - 16];
+			}
+			else {
+				led[0] = led[1] = led[2] = led[3] = pwm[x];
 			}
 		}
 
 		step = cnt % 256;
 
 		if (step == 0) {
-			gpioSetValue(RB_LED0, 1);
-			gpioSetValue(RB_LED1, 1);
-			gpioSetValue(RB_LED2, 1);
-			gpioSetValue(RB_LED3, 1);
+			if (led[0])
+				gpioSetValue(RB_LED0, 1);
+			if (led[1])
+				gpioSetValue(RB_LED1, 1);
+			if (led[2])
+				gpioSetValue(RB_LED2, 1);
+			if (led[3])
+				gpioSetValue(RB_LED3, 1);
 		}
 
 		if (step == led[0])
