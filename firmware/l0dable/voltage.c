7eea0a2f56ee71b3b8de1121e246123cc34c3bc3
Daniel Friesel <derf@finalrewind.org>
2011-12-30 21:29:43 +0100

    voltage: build average over 32 values. works pretty well.

--- firmware/l0dable/voltage.c
+++ firmware/l0dable/voltage.c
@@ -12,35 +12,46 @@
 /**************************************************************************/
 
 void ram(void) {
-	unsigned int v, mv, raw_mv;
+	unsigned char v;
+	unsigned long int cur_mv;
+	unsigned int mv[32];
+	unsigned char mv_idx = 0;
+	unsigned char i;
 	char not_charging;
 
-	mv = GetVoltage();
+	for (i = 0; i < 32; i++)
+		mv[i] = GetVoltage();
 
 	do {
 		lcdClear();
 		lcdPrintln("Battery status:");
 
 		not_charging = gpioGetValue(RB_PWR_CHRG);
-		raw_mv = GetVoltage();
+		cur_mv = GetVoltage();
 
 		if (!not_charging)
-			raw_mv -= 500;
+			cur_mv -= 500;
 
-		mv = ((mv * 5) + raw_mv) / 6;
-		v = mv/1000;
+		mv[++mv_idx % 32] = cur_mv;
+		cur_mv = 0;
+
+		for (i = 0; i < 32; i++)
+			cur_mv += mv[i];
+		cur_mv /= 32;
+
+		v = cur_mv/1000;
 
 		lcdNl();
 
 		if (!not_charging)
 			lcdPrintln("   CHARGING");
-		else if (mv < 3550)
+		else if (cur_mv < 3550)
 			lcdPrintln(" Charge NOW!");
-		else if (mv < 3650)
+		else if (cur_mv < 3650)
 			lcdPrintln(" Charge soon");
-		else if (mv < 4000)
+		else if (cur_mv < 4000)
 			lcdPrintln("      OK");
-		else if (mv < 4200)
+		else if (cur_mv < 4200)
 			lcdPrintln("     Good");
 		else
 			lcdPrintln("     Full");
@@ -49,9 +60,14 @@ void ram(void) {
 		lcdPrint(" ");
 		lcdPrint(IntToStr(v,2,0));
 		lcdPrint(".");
-		lcdPrint(IntToStr(mv % 1000, 3, F_ZEROS | F_LONG));
+		lcdPrint(IntToStr(cur_mv % 1000, 3, F_ZEROS | F_LONG));
 		lcdPrint("V  ~");
-		lcdPrint(IntToStr((mv - 3450) * 100 / (4100 - 3450), 2, F_ZEROS | F_LONG));
+
+		/* battery full */
+		if (cur_mv > 4200)
+			cur_mv = 4200;
+
+		lcdPrint(IntToStr((cur_mv - 3450) * 100 / (4100 - 3450), 2, F_ZEROS | F_LONG));
 		lcdPrintln("%");
 		lcdRefresh();
 	} while (getInputWaitTimeout(242) == BTN_NONE);
