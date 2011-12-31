d6533704a3b848acc41ea5c6c5adb8c7de67c1bf
Daniel Friesel <derf@finalrewind.org>
2011-12-31 14:22:41 +0100

    screensaver: nyan nyan nyan

--- firmware/l0dable/nick_image.c
+++ firmware/l0dable/nick_image.c
@@ -8,6 +8,7 @@
 
 #include "usetable.h"
 
+void next_image(unsigned char img);
 void ledwobble(signed char mode);
 
 const unsigned char pwm[32] = {
@@ -17,64 +18,93 @@ const unsigned char pwm[32] = {
 
 void ram(void)
 {
+	unsigned char i;
 	unsigned char t = 0;
-	unsigned int raw_mv = 0, mv = 0;
-	int not_charging;
-	unsigned int percent;
+	unsigned char img = 0;
 	char key;
+
+	unsigned char mv_idx = 0;
+	unsigned long int cur_mv;
+	unsigned int mv[32];
+	unsigned int percent;
+	int not_charging;
+
+	FIL nyanfile;
+	UINT readbytes;
+
 	lcdLoadImage("sr.lcd");
 	lcdRefresh();
+
+	f_open(&nyanfile, "nyan.lcd", FA_OPEN_EXISTING | FA_READ);
 
 	IOCON_PIO1_11 = 0x00;
 	IOCON_SWDIO_PIO1_3 = IOCON_SWDIO_PIO1_3_FUNC_GPIO;
 	gpioSetDir(RB_LED3, gpioDirection_Output);
 	gpioSetDir(1, 3, gpioDirection_Output);
 
-	mv = GetVoltage();
+	for (i = 0; i < 32; i++)
+		mv[i] = GetVoltage();
 
 	for (;;) {
 		if (++t >= 230) {
-			raw_mv = GetVoltage();
+			cur_mv = GetVoltage();
 			not_charging = gpioGetValue(RB_PWR_CHRG);
 
 			if (!not_charging)
-				raw_mv -= 500;
+				cur_mv -= 500;
 
-			mv = ((mv * 3) + raw_mv) / 4;
+			mv[++mv_idx % 32] = cur_mv;
+			cur_mv = 0;
 
-			percent = (mv - 3450) * 100 / (4100 - 3450);
+			for (i = 0; i < 32; i++)
+				cur_mv += mv[i];
+			cur_mv /= 32;
+
+			percent = (cur_mv - 3450) * 100 / (4100 - 3450);
 			setExtFont(GLOBAL(nickfont));
 			lcdClear();
 			DoString(0,0, GLOBAL(nickname));
 
+			if (percent > 100)
+				percent = 100;
+
 			if (!not_charging)
 				DoString(0, 40, "+");
-			else if (not_charging && (raw_mv > 4200)) {
+			else if (not_charging && (mv[mv_idx % 32] > 4200)) {
 				DoString(0, 40, "=");
 			}
 
-			DoString(16, 40, (percent > 100 ? "1" : ""));
+			DoString(16, 40, (percent == 100 ? "1" : ""));
 			DoString(32, 40, IntToStr(percent % 100, 2, F_ZEROS | F_LONG));
 			DoString(64, 40, "%");
 			lcdRefresh();
 
-			if (not_charging && (raw_mv < 4200))
+			if (not_charging && (mv[mv_idx % 32] < 4200))
 				ledwobble(-1);
 			else if (not_charging)
 				ledwobble(0);
 			else
 				ledwobble(1);
 
-			if (not_charging && (raw_mv < 4200)) {
+			if (not_charging && (mv[mv_idx % 32] < 4200)) {
 				t = 0;
-				lcdLoadImage("sr.lcd");
-				lcdRefresh();
+				img = (img + 1) % 3;
+				next_image(img);
 			}
 			else
-				t = 233;
+				t = 232;
 		}
 
-		delayms(80);
+		if ((img == 2) && (t % 2)) {
+			f_read(&nyanfile, (char *)lcdBuffer, RESX * RESY_B, &readbytes);
+			if (readbytes < (RESX * RESY_B)) {
+				f_lseek(&nyanfile, 0);
+				continue;
+			}
+			lcdDisplay();
+		}
+
+		delayms(60);
 
 		if (t%2)
 			gpioSetValue(1,3,1);
@@ -100,6 +130,21 @@ void ram(void)
 			return;
 	}
 };
+
+void next_image(unsigned char img)
+{
+	switch (img) {
+	case 0:
+		lcdLoadImage("sr.lcd");
+		break;
+	case 1:
+		lcdLoadImage("cccd.lcd");
+		break;
+	case 2:
+		break;
+	}
+	lcdRefresh();
+}
 
 void ledwobble(signed char mode)
 {
