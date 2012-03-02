c5eef2957c91d935d7ed3b6a940a97bf6d60c87c
Daniel Friesel <derf@finalrewind.org>
2012-03-02 12:44:01 +0100

    snd: 6bit output for use with resistors (no working hardware yet, though)

--- firmware/l0dable/snd.c
+++ firmware/l0dable/snd.c
@@ -8,15 +8,9 @@
 
 #include "usetable.h"
 
-const unsigned char pwm[16] = {0, 2, 3, 4, 6, 8, 11, 16, 23, 32, 45, 64,
-	90, 128, 181, 255};
-
 void ram(void)
 {
 	unsigned int i;
-	unsigned int cnt = 0;
-	unsigned const int cnt_max = 256;
-	unsigned int step;
 	unsigned char cur = 0;
 
 	FIL snd;
@@ -24,64 +18,60 @@ void ram(void)
 	unsigned char buf[2048];
 	unsigned int bufin = 0;
 	unsigned int fpos = sizeof(buf);
-
-	unsigned int con_2_4 = IOCON_PIO2_4;
-	unsigned int con_2_5 = IOCON_PIO2_5;
-	unsigned int con_3_2 = IOCON_PIO3_2;
-
-	IOCON_PIO2_4 = IOCON_PIO2_4_FUNC_GPIO;
-	IOCON_PIO2_5 = IOCON_PIO2_5_FUNC_GPIO;
-	IOCON_PIO3_2 = IOCON_PIO3_2_FUNC_GPIO;
+	unsigned int delay = 0;
 
 	gpioSetDir(RB_SPI_SS0, gpioDirection_Output);
 	gpioSetDir(RB_SPI_SS1, gpioDirection_Output);
+	gpioSetDir(RB_SPI_SS2, gpioDirection_Output);
 	gpioSetDir(RB_SPI_SS3, gpioDirection_Output);
+	gpioSetDir(RB_SPI_SS4, gpioDirection_Output);
+	gpioSetDir(RB_SPI_SS5, gpioDirection_Output);
+
+	gpioSetValue(RB_SPI_SS0, 0);
+	gpioSetValue(RB_SPI_SS1, 0);
+	gpioSetValue(RB_SPI_SS2, 0);
+	gpioSetValue(RB_SPI_SS3, 0);
+	gpioSetValue(RB_SPI_SS4, 0);
+	gpioSetValue(RB_SPI_SS5, 0);
 
 	f_open(&snd, "nyan.5nd", FA_OPEN_EXISTING | FA_READ);
 	f_read(&snd, buf, sizeof(buf), &readbytes);
 
 	while (getInputRaw() == BTN_NONE) {
 
-		if (++cnt >= cnt_max) {
-			cnt = 0;
+		if (++bufin == sizeof(buf)) {
+			bufin = 0;
+			f_read(&snd, buf, sizeof(buf), &readbytes);
+			fpos += readbytes;
+			if (readbytes < sizeof(buf))
+				break;
 
-			if (++bufin == sizeof(buf)) {
-				bufin = 0;
-				f_read(&snd, buf, sizeof(buf), &readbytes);
-				fpos += readbytes;
-				if (readbytes < sizeof(buf))
-					break;
+			lcdClear();
+			lcdPrint("fpos ");
+			lcdPrintln(IntToStr(fpos, 8, F_ZEROS | F_LONG));
+			lcdPrint(IntToStr(fpos / 4, 5, F_ZEROS | F_LONG));
+			lcdPrintln("ms");
+			lcdRefresh();
 
-				lcdClear();
-				lcdPrint("fpos ");
-				lcdPrintln(IntToStr(fpos, 8, F_ZEROS | F_LONG));
-				lcdPrint(IntToStr(fpos / 4, 5, F_ZEROS | F_LONG));
-				lcdPrintln("ms");
-				lcdRefresh();
-
-			}
-
-			cur = buf[bufin];
 		}
 
-		step = cnt % 256;
+		cur = buf[bufin];
 
-		if (step == 0) {
-			gpioSetValue(RB_SPI_SS0, 0);
-			gpioSetValue(RB_SPI_SS1, 1);
-		}
+		gpioSetValue(RB_SPI_SS5, (cur & 0x80) ? 1 : 0);
+		gpioSetValue(RB_SPI_SS4, (cur & 0x40) ? 1 : 0);
+		gpioSetValue(RB_SPI_SS3, (cur & 0x20) ? 1 : 0);
+		gpioSetValue(RB_SPI_SS2, (cur & 0x10) ? 1 : 0);
+		gpioSetValue(RB_SPI_SS1, (cur & 0x08) ? 1 : 0);
+		gpioSetValue(RB_SPI_SS0, (cur & 0x06) ? 1 : 0);
 
-		if (step == cur) {
-			gpioSetValue(RB_SPI_SS0, 1);
-			gpioSetValue(RB_SPI_SS1, 0);
-		}
-
+		delayms(1);
 	}
 
 	gpioSetValue(RB_SPI_SS0, 0);
 	gpioSetValue(RB_SPI_SS1, 0);
+	gpioSetValue(RB_SPI_SS2, 0);
+	gpioSetValue(RB_SPI_SS3, 0);
+	gpioSetValue(RB_SPI_SS4, 0);
+	gpioSetValue(RB_SPI_SS5, 0);
 
-	IOCON_PIO2_4 = con_2_4;
-	IOCON_PIO2_5 = con_2_5;
-	IOCON_PIO3_2 = con_3_2;
 };
